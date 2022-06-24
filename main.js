a8fd605210b4c48c901ca3ba09e6f8e486dee9da
Sai Shaket Kalivarapu <77451129+skalivarapu6@users.noreply.github.com>
2022-06-24 10:58:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById("myCanvas");
+canvas.height = window.innerHeight;
+canvas.width=200;
+
+//car drawing
+const ctx = canvas.getContext("2d");
+const car = new Car(100,100,30,50);
+car.draw(ctx);
+
+animate();
+
+function animate(){
+    car.update();
+    canvas.height = window.innerHeight;
+    car.draw(ctx);
+    
+    //Get animation frame by frame
+    requestAnimationFrame(animate);
+}
